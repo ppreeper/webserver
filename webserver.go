1fc36df17f135ce1cd4074794101e4ef3703aee2
Peter Preeper <ppreeper@gmail.com>
2017-10-25 17:06:00 -0600

    remove / from ./

--- webserver.go
+++ webserver.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./")))
+	http.Handle("/", http.FileServer(http.Dir(".")))
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
