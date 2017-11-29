21dff0c4df07549ddff30c5dc77729aeb3007254
Peter Preeper <ppreeper@gmail.com>
2017-11-29 09:00:42 -0700

    add port flag with default port 8080

--- webserver.go
+++ webserver.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+var port string
+var bindval string
+
+func init() {
+	flag.StringVar(&port, "port", "8080", "port to start")
+	flag.Parse()
+}
+
 func main() {
+	bindval = ":" + port
 	r := mux.NewRouter()
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir(".")))
 	http.Handle("/", r)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(bindval, nil); err != nil {
 		panic(err)
 	}
 }
