43d4692dbc188568c622d11532b95afe8df06b13
Peter Preeper <ppreeper@gmail.com>
2017-11-30 15:27:45 -0700

    dir flag incorrectly referring to port variable, corrected to dir variable

--- webserver.go
+++ webserver.go
@@ -8,16 +8,18 @@ import (
 )
 
 var port string
+var dir string
 var bindval string
 
 func init() {
 	flag.StringVar(&port, "port", "8080", "port to start")
+	flag.StringVar(&dir, "d", ".", "directory to serve")
 	flag.Parse()
 }
 
 func main() {
 	bindval = ":" + port
-	fs := http.FileServer(http.Dir("."))
+	fs := http.FileServer(http.Dir(dir))
 	mux := http.NewServeMux()
 	mux.Handle("/", fs)
 	withGz := gziphandler.GzipHandler(mux)
