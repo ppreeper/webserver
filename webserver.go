b7bc21e6a6085ceeb1a3a9cf2a9024c9ae001f26
Peter Preeper <ppreeper@gmail.com>
2018-05-10 15:04:00 -0600

    add MaxAge handler from https://github.com/gorilla/handlers/issues/81 to add expires headers to basic content

--- webserver.go
+++ webserver.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
+	"path/filepath"
+	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -24,7 +27,34 @@ func main() {
 	r := mux.NewRouter()
 	r.PathPrefix("/").Handler(fs)
 	http.Handle("/", r)
-	if err := http.ListenAndServe(bindval, handlers.CompressHandler(r)); err != nil {
+	if err := http.ListenAndServe(bindval, MaxAge(handlers.CompressHandler(r))); err != nil {
 		panic(err)
 	}
+}
+
+// MaxAge sets expire headers based on extension
+func MaxAge(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var age time.Duration
+		ext := filepath.Ext(r.URL.String())
+
+		// Timings are based on github.com/h5bp/server-configs-nginx
+
+		switch ext {
+		case ".rss", ".atom":
+			age = time.Hour / time.Second
+		case ".css", ".js":
+			age = (time.Hour * 24 * 365) / time.Second
+		case ".jpg", ".jpeg", ".gif", ".png", ".ico", ".cur", ".gz", ".svg", ".svgz", ".mp4", ".ogg", ".ogv", ".webm", ".htc":
+			age = (time.Hour * 24 * 30) / time.Second
+		default:
+			age = 0
+		}
+
+		if age > 0 {
+			w.Header().Add("Cache-Control", fmt.Sprintf("max-age=%d, public, must-revalidate, proxy-revalidate", age))
+		}
+
+		h.ServeHTTP(w, r)
+	})
 }
