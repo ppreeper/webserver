8d40cd7423fd4f95f74b8f07fbb2746f76d70c10
Peter Preeper <ppreeper@gmail.com>
2020-05-25 10:12:39 -0600

    move from chi router to go-fiber

--- go.mod
+++ go.mod
@@ -1,8 +1,12 @@
 module github.com/ppreeper/webserver
 
-go 1.12
+go 1.14
 
 require (
-	github.com/go-chi/chi v4.0.2+incompatible
-	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
+	github.com/gofiber/compression v0.0.3
+	github.com/gofiber/fiber v1.9.6
+	github.com/gofiber/helmet v0.0.3
+	github.com/gofiber/logger v0.0.9
+	github.com/gofiber/recover v0.0.5
+	github.com/klauspost/compress v1.10.6 // indirect
 )
--- webserver.go
+++ webserver.go
@@ -2,78 +2,68 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"net/http"
 	_ "net/http/pprof"
-	"path/filepath"
-	"time"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"github.com/gofiber/compression"
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/helmet"
+	"github.com/gofiber/logger"
+	"github.com/gofiber/recover"
 )
 
-var addr string
-var dir string
-var log bool
-var bindval string
-
 func main() {
+	var addr string
+	var dir string
+	var log bool
+	// var bindval string
 	flag.StringVar(&addr, "addr", ":8080", "addr to start")
 	flag.StringVar(&dir, "d", ".", "directory to serve")
 	flag.BoolVar(&log, "l", false, "enable logging")
 	flag.Parse()
 
-	r := chi.NewRouter()
-
-	// A good base middleware stack
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
+	app := fiber.New()
 	if log {
-		r.Use(middleware.Logger)
+		app.Use(logger.New())
 	}
-	r.Use(middleware.Recoverer)
-
-	// Set a timeout value on the request context (ctx), that will signal
-	// through ctx.Done() that the request has timed out and further
-	// processing should be stopped.
-	r.Use(middleware.Timeout(60 * time.Second))
-
-	// Compression on data
-	r.Use(middleware.DefaultCompress)
-
-	r.Mount("/debug", middleware.Profiler()) // ..routes return r })
-
-	fs := http.FileServer(http.Dir(dir))
-	r.Handle("/*", fs)
-
-	if err := http.ListenAndServe(addr, MaxAge(r)); err != nil {
-		panic(err)
+	app.Use(compression.New())
+	app.Use(helmet.New())
+	// Optional
+	cfg := recover.Config{
+		Handler: func(c *fiber.Ctx, err error) {
+			c.SendString(err.Error())
+			c.SendStatus(500)
+		},
 	}
+
+	app.Use(recover.New(cfg))
+
+	app.Static("/", dir)
+	app.Listen(addr)
 }
 
 // MaxAge sets expire headers based on extension
-func MaxAge(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var age time.Duration
-		ext := filepath.Ext(r.URL.String())
+// func MaxAge(h http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		var age time.Duration
+// 		ext := filepath.Ext(r.URL.String())
 
-		// Timings are based on github.com/h5bp/server-configs-nginx
+// 		// Timings are based on github.com/h5bp/server-configs-nginx
 
-		switch ext {
-		case ".rss", ".atom":
-			age = time.Hour / time.Second
-		case ".css", ".js":
-			age = (time.Hour * 24 * 365) / time.Second
-		case ".jpg", ".jpeg", ".gif", ".png", ".ico", ".cur", ".gz", ".svg", ".svgz", ".mp4", ".ogg", ".ogv", ".webm", ".htc":
-			age = (time.Hour * 24 * 30) / time.Second
-		default:
-			age = 0
-		}
+// 		switch ext {
+// 		case ".rss", ".atom":
+// 			age = time.Hour / time.Second
+// 		case ".css", ".js":
+// 			age = (time.Hour * 24 * 365) / time.Second
+// 		case ".jpg", ".jpeg", ".gif", ".png", ".ico", ".cur", ".gz", ".svg", ".svgz", ".mp4", ".ogg", ".ogv", ".webm", ".htc":
+// 			age = (time.Hour * 24 * 30) / time.Second
+// 		default:
+// 			age = 0
+// 		}
 
-		if age > 0 {
-			w.Header().Add("Cache-Control", fmt.Sprintf("max-age=%d, public, must-revalidate, proxy-revalidate", age))
-		}
+// 		if age > 0 {
+// 			w.Header().Add("Cache-Control", fmt.Sprintf("max-age=%d, public, must-revalidate, proxy-revalidate", age))
+// 		}
 
-		h.ServeHTTP(w, r)
-	})
-}
+// 		h.ServeHTTP(w, r)
+// 	})
+// }
