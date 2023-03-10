3b95187eb9a743e1a1d99ed8da78cc1f0d40eb78
Peter Preeper <ppreeper@gmail.com>
2023-03-10 01:49:39 -0700

    revert to fiber

--- webserver.go
+++ webserver.go
@@ -2,57 +2,31 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"net/http"
 	_ "net/http/pprof"
-	"strings"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
 	var addr string
 	var dir string
-	var logging bool
+	var log bool
+	// var bindval string
 	flag.StringVar(&addr, "addr", ":8080", "addr to start")
 	flag.StringVar(&dir, "d", ".", "directory to serve")
-	flag.BoolVar(&logging, "l", false, "enable logging")
+	flag.BoolVar(&log, "l", false, "enable logging")
 	flag.Parse()
 
-	r := chi.NewRouter()
-	if logging {
-		r.Use(middleware.Logger)
+	app := fiber.New()
+	if log {
+		app.Use(logger.New())
 	}
-	r.Use(middleware.Compress(5, "text/html", "text/css"))
-	r.Use(middleware.Recoverer)
+	app.Use(compress.New())
+	app.Use(recover.New())
 
-	filesDir := http.Dir(dir)
-	FileServer(r, "/", filesDir)
-
-	if err := http.ListenAndServe(addr, r); err != nil {
-		fmt.Println("webserver closed: %w", err)
-		return
-	}
-}
-
-// FileServer conveniently sets up a http.FileServer handler to serve
-// static files from a http.FileSystem.
-func FileServer(r chi.Router, path string, root http.FileSystem) {
-	if strings.ContainsAny(path, "{}*") {
-		panic("FileServer does not permit any URL parameters.")
-	}
-
-	if path != "/" && path[len(path)-1] != '/' {
-		r.Get(path, http.RedirectHandler(path+"/", http.StatusMovedPermanently).ServeHTTP)
-		path += "/"
-	}
-	path += "*"
-
-	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
-		rctx := chi.RouteContext(r.Context())
-		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
-		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
-		fs.ServeHTTP(w, r)
-	})
+	app.Static("/", dir)
+	app.Listen(addr)
 }
